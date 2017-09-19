9b5cda6a7801f710a5f6b5d4c26dbde0a599c6f8
Josh R. Dunlavy <dunlavy@users.noreply.github.com>
2017-09-18 23:32:44 -0700

    refactor(script.js): prepare for class

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-window.document.getElementById("textinputid").addEventListener("input", function (event) {
-	const button = document.getElementById("buttonid");
-	const userInput = event.target.value;
 
-	button.style.backgroundColor = 'electric mucus' === userInput.toLowerCase() ? 'aquamarine ' : userInput;
-	button.disabled = 'electric mucus' !== userInput.toLowerCase();
-});
