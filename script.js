26f1e0df01966417e95ada8aa797420abc9ffbf8
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-04-10 19:53:59 -0700

    worked on project

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+var queryText = document.querySelector("#textinputid").addEventListener("input", function(event) {
+    const buttonElement = document.querySelector("#buttonid");
+    const userInput = event.target.value;
 
+    buttonElement.style.backgroundColor = userInput.toLowerCase() === "electric mucus";
+
+    buttonElement.style.backgroundColor = isCorrect ? "green" : userInput;
+    buttonElement.disabled = !isCorrect;
+});
